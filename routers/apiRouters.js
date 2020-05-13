78743d31a22a5f3992c115d9d479506806a0d813
Ayesiza Hawah <yesiha2001@gmail.com>
2020-05-13 10:23:30 +0300

    feature(login a User): User can login [finishes #172690856]

--- routers/apiRouters.js
+++ routers/apiRouters.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { registerUser } = require('../controllers/userControllers');
+const { registerUser, loginUser } = require('../controllers/userControllers');
 const {  validation } = require('../middlewares/validations')
 
 const router = express.Router()
 
 router.post('/users/auth/signup',validation , registerUser);
+router.post('/users/auth/login',loginUser);
 
 
 
